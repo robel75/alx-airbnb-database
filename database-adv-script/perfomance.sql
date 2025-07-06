07e67394a6b9f672dd6373ff8fe8e03530551223
robel75 <robelisgone2@gmail.com>
2025-07-06 19:07:32 +0300

    Create perfomance.sql

---
+++ database-adv-script/perfomance.sql
@@ -0,0 +1,21 @@
+
+SELECT 
+    b.id AS booking_id,
+    b.start_date,
+    b.end_date,
+    b.status,
+    u.id AS user_id,
+    u.name AS user_name,
+    u.email AS user_email,
+    p.id AS property_id,
+    p.title AS property_title,
+    p.city,
+    p.price_per_night,
+    pay.id AS payment_id,
+    pay.amount,
+    pay.payment_date,
+    pay.status AS payment_status
+FROM Booking b
+JOIN "User" u ON b.user_id = u.id
+JOIN Property p ON b.property_id = p.id
+LEFT JOIN Payment pay ON pay.booking_id = b.id;
