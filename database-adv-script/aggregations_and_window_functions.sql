9a79f5ba7d0a71afe2587202ab3fac08a3ffa80c
Robel <robelisgone2@gmail.com>
2025-07-18 18:33:17 +0300

    updated

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -1,16 +1,9 @@
-SELECT users.id, users.first_name, users.last_name, COUNT(bookings.id) AS total_bookings
-FROM users
-LEFT JOIN bookings ON users.id = bookings.user_id
-GROUP BY users.id, users.first_name, users.last_name;
+USE AIRBNB;
 
-SELECT properties.id, properties.name, COUNT(bookings.id) AS booking_count,
-       RANK() OVER (ORDER BY COUNT(bookings.id) DESC) AS booking_rank
-FROM properties
-LEFT JOIN bookings ON properties.id = bookings.property_id
-GROUP BY properties.id, properties.name;
+SELECT USER.USER_ID, USER.FIRST_NAME,USER.LAST_NAME, COUNT(BOOKING.BOOKING_ID) AS TOTAL_BOOKINGS FROM USER
+LEFT JOIN BOOKING
+ON USER.USER_ID= BOOKING.USER_ID
+GROUP BY USER_ID;
 
-SELECT properties.id, properties.name, COUNT(bookings.id) AS booking_count,
-       ROW_NUMBER() OVER (ORDER BY COUNT(bookings.id) DESC) AS booking_position
-FROM properties
-LEFT JOIN bookings ON properties.id = bookings.property_id
-GROUP BY properties.id, properties.name;
+SELECT USER_ID, RANK() OVER (ORDER BY COUNT(BOOKING_ID) DESC)AS RANKING FROM BOOKING
+GROUP BY USER_ID
